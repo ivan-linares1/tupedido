6d3e41fb6f4418f2ed0eb157a86145ee618c6e84
Ivan <ivan.lianrrs@gmail.com>
2025-11-26 08:40:01 -0600

    tabla responsiva

--- resources/views/partials/tabla_pedidos.blade.php
+++ resources/views/partials/tabla_pedidos.blade.php
@@ -1,8 +1,9 @@
-<table class="table table-bordered table-striped m-8">
+<div class="table-responsive">
+    <table class="table table-bordered table-striped m-8">
         <thead class="table-info  text-center">
             <tr>
                 <th>Folio</th>
-                 @if(Auth::user()->rol_id != 3)<th>SAP</th>@endif {{--Esta columna la ven todos menos el cliente--}}
+                    @if(Auth::user()->rol_id != 3)<th>SAP</th>@endif {{--Esta columna la ven todos menos el cliente--}}
                 <th>Fecha</th>
                 <th>Cliente</th>
                 <th>Vendedor</th>
@@ -11,39 +12,40 @@
             </tr>
         </thead>
         <tbody id="tablaPedido">
-        @forelse($pedidos as $pedido)
-            <tr>
-                <td>
-                    <a href="{{ route('detallesP', $pedido->DocEntry) }}" style="cursor: pointer; color: blue; text-decoration: underline;" data-loading="true">
-                        PE - {{ $pedido->DocEntry }}
-                    </a>
-                    @if(Auth::user()->rol_id != 3)
-                    @if($pedido->DocStatus == 'A')
-                        <i class="bi bi-unlock-fill text-success ms-2" title="Pedido abierto"></i>
-                    @else
-                        <i class="bi bi-lock-fill text-danger ms-2" title="Pedido cerrado"></i>
-                    @endif @endif 
-                </td>
-                @if(Auth::user()->rol_id != 3)<td>{{ $pedido->DocNum ?? '---'}}</td>@endif {{--Esta columna la ven todos menos el cliente--}}
-                <td>{{ \Carbon\Carbon::parse($pedido->DocDate)->format('d-m-Y') }}</td>
-                <td>{{ $pedido->CardName }}</td>
-                <td>{{ $pedido->Vendedor->SlpName ?? '' }}</td>
-                <td>{{ number_format($pedido->Total,2) }} {{ $pedido->moneda->Currency ?? '' }}</td>
-                @if(in_array(Auth::user()->rol_id, [1, 2]))<td>
-                    @if( $pedido->DocNum ) Insertado en SAP 
-                    @else No insertado
-                    @endif
-                </td>@endif{{--Esta columna la ven solo super y administradores--}}
-            </tr>
-        @empty
-            <tr>
-                <td colspan="7" class="text-center">Sin pedidos disponibles</td>
-            </tr>
-        @endforelse
-    </tbody>
+            @forelse($pedidos as $pedido)
+                <tr>
+                    <td>
+                        <a href="{{ route('detallesP', $pedido->DocEntry) }}" style="cursor: pointer; color: blue; text-decoration: underline;" data-loading="true">
+                            PE - {{ $pedido->DocEntry }}
+                        </a>
+                        @if(Auth::user()->rol_id != 3)
+                        @if($pedido->DocStatus == 'A')
+                            <i class="bi bi-unlock-fill text-success ms-2" title="Pedido abierto"></i>
+                        @else
+                            <i class="bi bi-lock-fill text-danger ms-2" title="Pedido cerrado"></i>
+                        @endif @endif 
+                    </td>
+                    @if(Auth::user()->rol_id != 3)<td>{{ $pedido->DocNum ?? '---'}}</td>@endif {{--Esta columna la ven todos menos el cliente--}}
+                    <td>{{ \Carbon\Carbon::parse($pedido->DocDate)->format('d-m-Y') }}</td>
+                    <td>{{ $pedido->CardName }}</td>
+                    <td>{{ $pedido->Vendedor->SlpName ?? '' }}</td>
+                    <td>{{ number_format($pedido->Total,2) }} {{ $pedido->moneda->Currency ?? '' }}</td>
+                    @if(in_array(Auth::user()->rol_id, [1, 2]))<td>
+                        @if( $pedido->DocNum ) Insertado en SAP 
+                        @else No insertado
+                        @endif
+                    </td>@endif{{--Esta columna la ven solo super y administradores--}}
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="7" class="text-center">Sin pedidos disponibles</td>
+                </tr>
+            @endforelse
+        </tbody>
     </table>
+</div>
 
-    <!-- Paginación -->
-    <div class="d-flex justify-content-center mt-3">
-        {{ $pedidos->appends(request()->query())->links('pagination::bootstrap-5') }}
-    </div>
+<!-- Paginación -->
+<div class="d-flex justify-content-center mt-3">
+    {{ $pedidos->appends(request()->query())->links('pagination::bootstrap-5') }}
+</div>
